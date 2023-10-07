13ae82001597e9fb0ea998cd3b16ec6df00b9239
ryanzambrano <rez1237@gmail.com>
2023-10-07 14:34:45 -0400

    deleting images now available

--- components/closet.js
+++ components/closet.js
@@ -6,56 +6,104 @@ import {
   SafeAreaView,
   FlatList,
   Image,
+  TouchableOpacity,
 } from "react-native";
 import { supabase } from "./auth/supabase";
+import Icon from "react-native-vector-icons/FontAwesome";
+
 const ClosetUI = ({ route }) => {
   const { session } = route.params;
   const [photos, setPhotos] = useState([]);
 
-  useEffect(() => {
-    const fetchPhotoTimestampsFromDatabase = async () => {
-      if (!session || !session.user) {
-        console.error("No active session found.");
-        return;
+  const deletePictures = async (lastModified) => {
+    try {
+      const filename = `${session.user.id}/${session.user.id}-${lastModified}`;
+
+      const { data: removeData, error: removeError } = await supabase.storage
+        .from("user_pictures")
+        .remove(filename);
+
+      if (removeError) {
+        throw removeError;
       }
 
-      // Fetch last_modified timestamps for the user's images from the table
-      const { data, error } = await supabase
+      const { data: removeRow, error: rowError } = await supabase
         .from("user_images")
-        .select("last_modified")
-        .eq("user_id", session.user.id);
+        .delete()
+        .eq("user_id", session.user.id)
+        .eq("last_modified", lastModified);
 
-      if (data) {
-        const base_url =
-          "https://vzdnrdsqkzwzihnqfong.supabase.co/storage/v1/object/public/user_pictures";
-
-        // Construct the URLs based on the fetched last_modified dates
-        const photoUrls = data.map(
-          (image) =>
-            `${base_url}/${session.user.id}/${session.user.id}-${image.last_modified}`
-        );
-
-        setPhotos(photoUrls);
-        console.log(photoUrls[1]);
-      } else {
-        console.error("Failed to fetch image metadata from database:", error);
+      if (rowError) {
+        throw rowError;
       }
-    };
+
+      if (removeData && removeRow) {
+      }
+
+      if (removeError) {
+        alert(error.message);
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  const fetchPhotoTimestampsFromDatabase = async () => {
+    if (!session || !session.user) {
+      console.error("No active session found.");
+      return;
+    }
+
+    // Fetch last_modified timestamps for the user's images from the table
+    const { data, error } = await supabase
+      .from("user_images")
+      .select("last_modified")
+      .eq("user_id", session.user.id);
+
+    if (data) {
+      const base_url =
+        "https://vzdnrdsqkzwzihnqfong.supabase.co/storage/v1/object/public/user_pictures";
+
+      const enrichedData = data.map((image) => ({
+        ...image,
+        url: `${base_url}/${session.user.id}/${session.user.id}-${image.last_modified}`,
+      }));
+
+      setPhotos(enrichedData); // Store the entire data array
+    } else {
+      console.error("Failed to fetch image metadata from database:", error);
+    }
+  };
+
+  useEffect(() => {
     fetchPhotoTimestampsFromDatabase();
   }, []);
 
+  const renderDelete = (lastModified) => (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={async () => {
+        await deletePictures(lastModified);
+        await fetchPhotoTimestampsFromDatabase();
+      }}
+    >
+      <Icon name="times" size={25} color="grey" />
+    </TouchableOpacity>
+  );
   return (
     <SafeAreaView style={styles.safeArea}>
       <Text style={styles.mainHeader}>My Closet</Text>
       <FlatList
         data={photos}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <Image
-            source={{ uri: item }}
-            style={styles.image}
-            onError={(error) => console.log("Error loading image:", error)}
-          />
+        renderItem={({ item, index }) => (
+          <View style={styles.imageContainer}>
+            {renderDelete(item.last_modified)}
+            <Image
+              source={{ uri: item.url }}
+              style={styles.image}
+              onError={(error) => console.log("Error loading image:", error)}
+            />
+          </View>
         )}
       />
     </SafeAreaView>
