ef921f99b75ed351d80afcfb48ba26c947bdb4c3
mattbrwnn <mattb7854@icloud.com>
2023-10-08 05:14:38 -0400

    h

--- components/makeMyOutfit.js
+++ components/makeMyOutfit.js
@@ -20,7 +20,7 @@ const MakeMyOutfitUI = ({ route }) => {
   const { session } = route.params;
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-
+  const [currentPrompt, setCurrentPrompt] = useState(0);
   const [inputText, setInputText] = useState("");
   const [translatedResponse, setTranslatedResponse] = useState("");
   const [wardrobeString, setWardrobeString] = useState("");
@@ -33,6 +33,7 @@ const MakeMyOutfitUI = ({ route }) => {
       return (
         <View style={styles.loadingDots}>
           <LoadingDots size={8} colors={["grey", "gold", "grey", "gold"]} />
+          <Text style={{marginTop: 50, color: 'white', fontWeight: 'bold'}}>Generating...</Text>
         </View>
       );
     }
@@ -174,6 +175,42 @@ const MakeMyOutfitUI = ({ route }) => {
     }
   };
 
+  const promptIdeas = [
+    "Give me an outfit for the movies?",
+    "What's a good outfit for an emo-themed party?",
+    "I'm going to the beach, what should I wear?",
+    "I need a outfit for a wedding, what do I wear?",
+    "Suggest an outfit for a first date at a casual cafe.",
+    "I need a look for a beach party this weekend.",
+    "What should I wear to a formal business meeting?",
+    "Recommend an outfit for a winter wedding.",
+    "What's the best attire for a summer music festival?",
+    "Help me choose clothes for a hike in the mountains.",
+    "I'm attending a retro '80s party. What should I wear?",
+    "Can you suggest a comfortable yet stylish airport look?",
+    "I'm going for a yoga class. What outfit do you recommend?",
+    "What's a trendy outfit for a day of shopping in the city?",
+    "Help me pick an attire for a charity gala.",
+    "Recommend a look for a brunch with friends.",
+    "I'm going to a book launch event. What should I wear?",
+    "Suggest a sporty look for a day at the racetrack.",
+    "I'm attending a Broadway show tonight. What's the best outfit?",
+    "What's a cozy outfit for a rainy day indoors?",
+    "Recommend a stylish look for a visit to an art gallery.",
+    "I'm going for a picnic in the park. What should I wear?",
+    "Can you suggest an attire for a night out at a jazz club?",
+    "Help me pick a look for a family barbecue.",
+  ];
+
+   const renderEmptyList = () => {
+    return (
+        <View style={styles.emptyListContainer}>
+            <Text style={styles.emptyListText}>Need inspiration? Try asking:</Text>
+            <Text style={styles.suggestionText}>{promptIdeas[currentPrompt]}</Text>
+        </View>
+    );
+};
+
   const RenderPhotos = () => {
     if (!photoUrls || photoUrls.length === 0) {
       return null;
@@ -192,9 +229,10 @@ const MakeMyOutfitUI = ({ route }) => {
   };
 
   return (
-    <SafeAreaView
-      style={{ flex: 1, backgroundColor: "#1D1D20", marginBottom: 80 }}
-    >
+    <SafeAreaView style={{flex: 1, backgroundColor: "#1D1D20", marginBottom: 80,}}>
+    <View style={styles.header}>
+  <Text style={styles.headerText}>Build an Outfit</Text>
+    </View>
       <KeyboardAvoidingView
         style={styles.container}
         behavior={Platform.OS === "ios" ? "padding" : null}
@@ -204,6 +242,7 @@ const MakeMyOutfitUI = ({ route }) => {
         <View style={styles.messagesContainer}>
           <FlatList
             data={messages}
+            ListEmptyComponent={renderEmptyList}
             ref={flatListRef}
             renderItem={({ item, index }) => (
               <View style={{ flexDirection: "column" }}>
@@ -257,11 +296,17 @@ const styles = StyleSheet.create({
   },
   header: {
     flexDirection: "row",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
-    marginBottom: 10,
+    marginBottom: 0,
     paddingHorizontal: 10,
+    marginTop: 5,
   },
+  headerText: {
+    fontSize: 20,  
+    fontWeight: 'bold',
+    color: 'white',
+},
   messagesContainer: {
     flex: 1,
   },
@@ -335,6 +380,27 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
   },
+  emptyListContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100,
+  },
+  emptyListText: {
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+    paddingHorizontal: 10,
+    marginBottom: 15,
+  },
+  
+  suggestionText: {
+    fontSize: 16,
+    color: '#888',
+    paddingHorizontal: 10,
+    textAlign: 'center',
+    marginBottom: 5,
+  }
   //#cd9625
 });
 export default MakeMyOutfitUI;
